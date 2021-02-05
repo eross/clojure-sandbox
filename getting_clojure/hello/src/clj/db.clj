24a39d4f29fcb0debf5e7e1f08f1193302118b38
Eric Ross <epr@rossmbox.com>
2021-02-05 07:43:22 -0800

    Fixed hello directory tree

--- getting_clojure/hello/src/clj/db.clj
+++
@@ -1,8 +0,0 @@
-(ns hello.core)
-(require '[clojure.java.jdbc :as jdbc])
-(def conn {:dbtype "derby" :dbame "./books" :create true})
-
-(def db {:dbtype "derby" :dbame "./books"})
-
-(jdbc/query db ["select * from books"])
-
