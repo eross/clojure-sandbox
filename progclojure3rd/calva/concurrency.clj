3b70697066914d98582d38d7d5149f06498f786a
Eric Ross <eric.ross@@hp.com>
2023-05-04 16:34:37 -0700

    More ref stuff.  Add luminous project

--- progclojure3rd/calva/concurrency.clj
+++ progclojure3rd/calva/concurrency.clj
@@ -1,2 +1,24 @@
 (ns concurrency)
 
+(comment
+  (ref 'initial-state)
+  (def current-track (ref "Mars, the Bringer of War"))
+  (deref current-track)
+  @current-track
+  (dosync (ref-set current-track "Eric the bringer of code"))
+
+  (def messages (ref ()))
+
+  (defn bad-add-message [msg]
+    (dosync (ref-set messages (cons msg @messages))))
+  
+ (defn add-message [msg]
+   (dosync (alter messages conj msg)))
+  
+  @messages
+
+  (add-message "hi")
+
+  @messages
+  :fcf
+  )
--- progclojure3rd/calva/specs.clj
+++ progclojure3rd/calva/specs.clj
@@ -1,4 +1,23 @@
 (ns specs
-  (:require [clojure.spec.alpha]) 
+  (:require [clojure.spec.alpha :as s]) 
   )
 
+(s/def :my.app/company-name string?)
+
+(s/valid? :my.app/company-name "Acme Moving")
+
+(s/valid? :my.app/company-name 101)
+
+(s/def :marble/color #{:red :green :blue})
+
+(s/valid? :marble/color :yello)
+
+(s/int-in 0 11)
+
+(s/def ::ranged-roll (s/int-in 0 11))
+
+(s/def ::odd-int (s/and int? odd?))
+(s/valid? ::odd-int 5)
+
+(s/valid? ::odd-int 4)
+(s/explain ::odd-int 4)
