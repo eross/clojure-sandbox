b0bc68796e6836d0a1e9ee2af0dcb20f3bfcac4b
Eric Ross <epr@rossmbox.com>
2023-12-05 16:48:54 -0800

    Start Advent 2

---
+++ func_design/spec/adventcode/advent2_spec.clj
@@ -0,0 +1,5 @@
+(ns adventcode.advent2-spec
+   (:require [speclj.core :refer :all]
+            [func-design.core :refer :all]
+            [clojure.string :as str]
+            [adventcode.advent2 :refer :all]))
---
+++ func_design/src/adventcode/advent2.clj
@@ -0,0 +1,1 @@
+(ns adventcode.advent2)
