b7650c669b16b010ae4138e1d176a1cfc61d4d3f
Eric Ross <epr@rossmbox.com>
2025-05-21 11:36:09 -0700

    More practice--atoms

--- getting_clojure/hello/src/hello/sandbox.clj
+++ getting_clojure/hello/src/hello/sandbox.clj
@@ -3,12 +3,12 @@
 
 (apply str (intr \, "abc"))
 
-(take 30 
-      (for 
-       [x (range 10000000) 
+(take 30
+      (for
+       [x (range 10000000)
         y (range 10000000)
         :while (< y x)]
-        [ x y]))
+        [x y]))
 
 (def numbers [10 20 30 40 50])
 
@@ -20,4 +20,33 @@
 (reduce + 0 numbers)
 
 (reduce + numbers)
+
+(def at (atom 10))
+(type at)
+(def nat 10)
+(type nat)
+
+
+at
+nat
+
+(def prom (promise))
+
+(deliver prom 44)
+
+prom
+
+(defn watcher [key ref old new]
+  (-> (println "watched")
+      (println "key:" key)
+      (println "ref:" ref)
+      (println "old: " old)
+      (println "new: " new)
+      ))
+
+
+(add-watch at :logger watcher)
+; (swap! nat + 10) ; This will throw an error because 'nat' is not an atom.
+(swap! at + 10)
+refer
 
