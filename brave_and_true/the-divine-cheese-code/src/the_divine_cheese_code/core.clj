22560941cdc175740b6b7ba3656ad454d499354b
Eric Ross <epr@rossmbox.com>
2026-03-07 11:19:59 -0800

    fixed syntax errors

--- brave_and_true/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ brave_and_true/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -8,43 +8,39 @@
               :cheese-name "Archbishop Hildebold's Cheese Pretzel"
               :lat 50.95
               :lng 6.97}
-              {:location "Zurich, Switzerland"
-                            :cheese-name "The Standard Emmental"
-                            :lat 47.37
-                            :lng 8.55}
-              {:location "Marseille, France"
-                            :cheese-name "Le Fromage de Cosquer"
-                            :lat 43.30
-                            :lng 5.37}
-              {:location "Zurich, Switzerland"
-                            :cheese-name "The Lesser Emmental"
-                            :lat 47.37
-                            :lng 8.55}
-              {:location "Vatican City"
-                            :cheese-name "The Cheese of Turin"
-                            :lat 41.90
-                            :lng 12.45}])
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Standard Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Marseille, France"
+              :cheese-name "Le Fromage de Cosquer"
+              :lat 43.30
+              :lng 5.37}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Lesser Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Vatican City"
+              :cheese-name "The Cheese of Turin"
+              :lat 41.90
+              :lng 12.45}])
 
 
 (defn -main
   [& args]
-  (println (svg/points heists))
-
-42
+  (println (svg/points heists)))
 
 (defmacro backwards
   [form]
   (reverse form))
 
-  (backwards (" backwards" " am" "I" str))
+(backwards (" backwards" " am" "I" str))
 
- (def addition-list (list + 1 2))
- addition-list
-  
- (eval addition-list)
-  
- (concat '(3) [10])
- (concat addition-list [10])
- (eval (concat addition-list [10]))
- (eval addition-list [3])
+(def addition-list (list + 1 2))
+addition-list
 
+(eval addition-list)
+
+(concat '(3) [10])
+(concat addition-list [10])
+(eval (concat addition-list [10]))
--- brave_and_true/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ brave_and_true/the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -2,6 +2,11 @@
 
 (defn latlng->point
   "Convert lat/lng map to comma separated string"
-  (latlng)
+  [latlng]
   (str (:lng latlng) "," (:lat latlng)))
+
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
 
