3d8776895ba5ff35771743c0a77c25c17489ea8b
Eric Ross <epr@rossmbox.com>
2026-03-15 09:26:54 -0700

    Finished book

--- brave_and_true/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ brave_and_true/the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -53,3 +53,44 @@
 
 
 (macroexpand '(andx 1 2 3 4)) 
+  
+
+ ;; threads
+ (future (Thread/sleep 40000)
+         (println "I'll print after 4 seconds"))
+ (println "I'll print immediately")
+
+ (let [result (future (println "this prints once") 
+                      (+ 1 1))] 
+   (println "deref: " (deref result)) 
+   (println "@: " @result))
+
+(let [result (future (Thread/sleep 3000) 
+                     (+ 1 1))] 
+  (println "The result is: " @result) 
+  (println "It will be at least 3 seconds before I print"))
+
+(realized? (future (Thread/sleep 1000)))
+
+(let [f (future)]
+  @f
+  (realized? f))
+ 
+ ;; delay
+(def jackson-5-delay
+  (delay (let [message "Just call my name and I'll be there"]
+           (println "First deref: " message))))
+ 
+(force jackson-5-delay)
+@jackson-5-delay
+
+ ;;promise
+ 
+ (def my-promise (promise))
+ (deliver my-promise (+ 1 2))
+ @my-promise
+
+(let [ferengi-wisdom-promise (promise)] 
+  (future (println "Here's some Ferengi wisdom:" @ferengi-wisdom-promise)) 
+  (Thread/sleep 1000) 
+  (deliver ferengi-wisdom-promise "Whisper your way to success."))
